fe299b68c9d9c025a8c61108a1dc97642d88adff
Adriana Ceron <adrianaceron@Adreana.home>
2020-08-12 22:51:54 +0200

    ejercicio 2F terminado

--- week-1/Homework/F-strings-methods/exercise2.js
+++ week-1/Homework/F-strings-methods/exercise2.js
@@ -1,3 +1,11 @@
-const name = " Daniel  ";
+let message = "Hello my name is Daniel and my name is "
+let name = " Daniel  ".trim();
+let nameLength = name.length;
+let message2 = " characters long"
+console.log(message + nameLength + message2);
 
-console.log(message);
+
+
+
+
+
