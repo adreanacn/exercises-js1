4891ceed5122d160a93d57a24cdae89b25c5cee4
Adriana Ceron <adrianaceron@Adreana.home>
2020-08-14 00:03:27 +0200

    ejercicios K completados

--- week-1/Homework/K-functions-parameters/exercise5.js
+++ week-1/Homework/K-functions-parameters/exercise5.js
@@ -1,5 +1,20 @@
 // Declare your function here
 
-const greeting = createLongGreeting("Daniel", 30);
+function createLongGreeting(name, age) {
+    return "My name is " + name + " and I'm " + age + " years old";
+}
 
+const greeting = createLongGreeting("Daniel", 30);
 console.log(greeting);
+
+
+
+
+
+
+
+
+
+
+
+
