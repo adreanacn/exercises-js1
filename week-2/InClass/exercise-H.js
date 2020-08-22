10c7b314b508db8fa97d69d6f1123a431585d0f6
Adriana Ceron <adrianaceron@Adreana.home>
2020-08-22 11:59:36 +0200

    ejercicio H InClass

--- week-2/InClass/exercise-H.js
+++ week-2/InClass/exercise-H.js
@@ -5,3 +5,15 @@ function exponential(number) {
 function isEven(number) {
   return number % 2 === 0;
 }
+
+for (var number = 5; number <= 20; number++) {
+  if (isEven(number)) {
+    console.log("Este es el exponencial " + exponential(number), "Este es el numero usado " + number);
+  }
+
+}
+
+
+
+
+
