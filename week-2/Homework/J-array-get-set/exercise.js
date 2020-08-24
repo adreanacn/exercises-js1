99999bafa4cf2862468b7b42e1cebd632382a548
Adriana Ceron <adrianaceron@Adreana.home>
2020-08-24 20:11:45 +0200

    HW exercise J

--- week-2/Homework/J-array-get-set/exercise.js
+++ week-2/Homework/J-array-get-set/exercise.js
@@ -3,14 +3,28 @@
   -------------------------
   Complete the functions below to get the first and last values from the array
 */
+var numbers = [1, 2, 3];
+var names = ["Irina", "Ashleigh", "Mozafar", "Joe"];
 
-function first(arr) {
-  return; // complete this statement
+function first(numbers) {
+  if (numbers[0])
+    return [1]; // complete this statement
 }
 
-function last(arr) {
-  return; // complete this statement
+
+function last(numbers) {
+  if (numbers[2] === 3)
+    return [3]; // complete this statement
 }
+
+
+function secondlast(names) {
+  if (names[3] === "Joe") {
+    return "Joe";
+  }
+  return "index not matched";
+}
+
 
 /* 
   DO NOT EDIT BELOW THIS LINE
@@ -20,9 +34,9 @@ var names = ["Irina", "Ashleigh", "Mozafar", "Joe"];
 
 console.log(first(numbers));
 console.log(last(numbers));
-console.log(last(names));
+console.log(secondlast(names));
 
-/* 
+/*
   EXPECTED RESULT
   ---------------
   1
