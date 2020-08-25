c87f0c585552dfbbd8f551eee24ece75d771de0f
Adriana Ceron <adrianaceron@Adreana.home>
2020-08-25 18:49:09 +0200

    InClass exercise A

---
+++ week-3/InClass/exercise-A.js
@@ -0,0 +1,14 @@
+const students = [
+    "Ana",
+    "Daniel",
+    "Mariana",
+    "Javier "
+];
+
+console.log(students + students.length);
+
+students.unshift("Raquel");
+console.log(students + students.length);
+
+students.push("Roberto ");
+console.log(students + students.length);
